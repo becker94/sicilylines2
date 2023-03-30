65e4c6881b915527bcbbb4d0fdb47ee2a5b7058e
becker94 <toureben202@gmail.com>
2023-03-30 13:10:07 +0200

    oe c bon

--- src/Form/LiaisonType.php
+++ src/Form/LiaisonType.php
@@ -3,19 +3,30 @@
 namespace App\Form;
 
 use App\Entity\Liaison;
+use App\Entity\Port;
+use App\Entity\Secteur;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 class LiaisonType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('duree')
-            ->add('secteur')
-            ->add('port_depart')
-            ->add('port_arrive')
+            ->add('port_depart', EntityType::class, [
+                'class'=> Port::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('port_arrive', EntityType::class, [
+                'class'=> Port::class,
+                'choice_label' => 'nom',
+            ])
+            ->add('secteur', EntityType::class, [
+                'class'=> Secteur::class,
+                'choice_label' => 'libelle',
+            ])
         ;
     }
 
